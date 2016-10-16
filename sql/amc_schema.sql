75ca944075c4ceaacb0e59a0a4eaacb5d8d6ebd0
Derek Cline <derek.cline@ticketmaster.com>
2016-10-16 15:51:16 -0700

    Commented out inserts and selects

--- sql/amc_schema.sql
+++ sql/amc_schema.sql
@@ -10,7 +10,9 @@ create table user (
      phone tinytext not null
 );
 
-insert into user (first_name, middle_initial, last_name, email, phone) values ("Erik", "", "Tank", "erik2029@gmail.com", "480-692-1894");
+-- insert into user (first_name, middle_initial, last_name, email, phone) values ("Erik", "", "Tank", "erik2029@gmail.com", "480-692-1894");
+
+-- select id, first_name, middle_initial, last_name, email, phone from user;
 
 
 -- attribute
@@ -23,8 +25,9 @@ create table attribute (
      description text
 );
 
-insert into attribute (grouping_id, name, display_name, description)
-values (1, "grouping", "Grouping Name", "This is a grouping.");
+-- insert into attribute (grouping_id, name, display_name, description) values (1, "grouping", "Grouping Name", "This is a grouping.");
+
+-- select id, grouping_id, name, display_name, description from attribute;
 
 
 -- user_attribute
@@ -38,8 +41,7 @@ create table user_attribute (
      -- primary int(1) not null default 1
 );
 
-insert into user_attribute (user_id, attribute_id, value, comment)
-values (1, 1, "User Attribute", "Comment.");
+-- insert into user_attribute (user_id, attribute_id, value, comment) values (1, 1, "User Attribute", "Comment.");
 
 
 -- grouping
@@ -51,8 +53,7 @@ create table grouping (
      description text
 );
 
-insert into grouping (name, display_name, description)
-values ("grouping", "Grouping", "Description");
+-- insert into grouping (name, display_name, description) values ("grouping", "Grouping", "Description");
 
 
 -- grouping_option
@@ -65,6 +66,5 @@ create table grouping_option (
      description text
 );
 
-insert into grouping_option (grouping_id, option_name, display, description)
-values (1, "option_name", "Option", "Description.");
+-- insert into grouping_option (grouping_id, option_name, display, description) values (1, "option_name", "Option", "Description.");
 
